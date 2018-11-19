bbd135b388e52f5a0a9ebb937359410a132c5086
Jasmine Smith <jasmine.smith28@outlook.com>
2018-11-19 14:17:43 +0000

    product page outline

---
+++ themes/inhabitant/single-product.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying all single posts.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+
+	<section class="shopping-product">
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'template-parts/content', 'single' ); ?>
+
+			<?php the_post_navigation(); ?>
+
+
+		<?php endwhile; // End of the loop. ?>
+		</section>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
