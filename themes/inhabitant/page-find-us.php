8e4e5ce317d2b3f1f3c5ecc86bd01f9eb0d050d5
Jasmine Smith <jasmine.smith28@outlook.com>
2018-11-20 17:30:47 +0000

    find us page

--- themes/inhabitant/page-find-us.php
+++ themes/inhabitant/page-find-us.php
@@ -10,13 +10,17 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main find-us" role="main">
 
+		<section class="findUs">
 			<?php while ( have_posts() ) : the_post(); ?>
-
+			<div class="findUsLink">
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
 			<?php endwhile; // End of the loop. ?>
-			
-			<?php get_sidebar(); ?>
+				</div>
+			<div class="widgetSidebar">
+              <?php get_sidebar(); ?>
+		  </div>
+		</section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
--- themes/inhabitant/single-post.php
+++ themes/inhabitant/single-post.php
@@ -10,25 +10,36 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php while ( have_posts() ) : the_post(); ?>
+	<section class="singleJournal">
 
-		<?php the_post_thumbnail();?>
-<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-   <p><?php echo get_the_date();?> /
-  <?php echo $post->comment_count; ?> COMMENTS/ BY <?php the_author(); ?></p>
-  <p><?php the_content(); ?></p>
+		<?php while ( have_posts() ) : the_post(); ?>
+		<div class="singleJournalLink">
+		<div class= "journalSingleImage">
+      <div class="journalSingleTitle"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></div>
+          <?php the_post_thumbnail();?>
+		<div class="journalSingleInfo"><p><?php echo get_the_date();?> / <?php echo $post->comment_count; ?> Comments / By <?php the_author(); ?></p></div>
+		  <p><?php the_content(); ?></p>
+		  <div class="socialMedia">
+				<button><i class="fab fa-facebook-f"></i> LIKE</button>
+				<button><i class="fab fa-twitter"></i> TWEET</button>
+				<button ><i class="fab fa-pinterest"></i> PIN</button>
+				</div>
+		  </div>
 
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) :
 					comments_template();
 				endif;
-			?>
+				?>
 
 		<?php endwhile; // End of the loop. ?>
+		</div>
 
+		<div class="widgetSidebar">
+              <?php get_sidebar(); ?>
+		  </div>
+		</section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
